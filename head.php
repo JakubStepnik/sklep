84320432d78d25b125d3f9adfc7217e76cd8e2e0
Jakub Stepnik <jakubstepni1k@gmail.com>
2020-11-21 20:15:44 +0100

    pracownik,admin logowanie zmiana w navbarze

--- head.php
+++ head.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+require_once 'dolacz/db.inc.php';
 ?>
 <!DOCTYPE html>
 <html>
--- nav.php
+++ nav.php
@@ -45,12 +45,25 @@
         <img class="nav-item " src="login.svg" width="30px" height="30px">
         </a>
         <?php 
-          if(isset($_SESSION["useruid"])){ echo'
+          if(isset($_SESSION["perm"])){
+            $sql="Select*from mods";
+            $result=mysqli_query($conn,$sql);$row=mysqli_fetch_assoc($result); 
+            if($row["perm"]==0){
+              echo '
         <div class="dropdown-menu"  aria-labelledby="navbarDropdown" style="left:-100%; background-color:#292F5A ;">
-          <a class="dropdown-item lato" href="ustawienia.php" style="font-size:16px; color:white;margin-left:2px;background-color:#292F5A;">Ustawienia</a>
+          <a class="dropdown-item lato" href="panel_adm.php" style="font-size:16px; color:white;margin-left:2px;background-color:#292F5A; font-algin:center">Panel admina</a>
           <a class="dropdown-item lato" href="dolacz/wyloguj.inc.php" style="font-size:16px; color:white;margin-left:2px; background-color:#292F5A;">Wyloguj</a>
           </div>';
-          } else if(isset($_SESSION["perm"])){echo'
+          }
+          else if($row["perm"]==1){
+            echo '
+      <div class="dropdown-menu"  aria-labelledby="navbarDropdown" style="left:-100%; background-color:#292F5A ;">
+        <a class="dropdown-item lato" href="panel_adm.php" style="font-size:16px; color:white;margin-left:2px;background-color:#292F5A; font-algin:center">Panel pracownika</a>
+        <a class="dropdown-item lato" href="dolacz/wyloguj.inc.php" style="font-size:16px; color:white;margin-left:2px; background-color:#292F5A;">Wyloguj</a>
+        </div>';
+        }
+      } else if(isset($_SESSION["useruid"])){
+          echo'
         <div class="dropdown-menu"  aria-labelledby="navbarDropdown" style="left:-100%; background-color:#292F5A ;">
           <a class="dropdown-item lato" href="ustawienia.php" style="font-size:16px; color:white;margin-left:2px;background-color:#292F5A;">Ustawienia</a>
           <a class="dropdown-item lato" href="dolacz/wyloguj.inc.php" style="font-size:16px; color:white;margin-left:2px; background-color:#292F5A;">Wyloguj</a>
